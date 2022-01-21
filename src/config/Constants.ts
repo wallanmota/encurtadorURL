8830e496883ce93fb4fc72a482e0ad03e67c053e
wallanmota <wallanmota.13@gmail.com>
2022-01-21 20:15:42 -0300

    projeto finalizado

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://admin:<password>@cluster0.98jgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
